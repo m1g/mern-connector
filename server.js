84f0302060d1ffac127a1d8cfbd63e14b2ace6ab
m1g <miguel.malcolm@spatialnetworks.com>
2019-07-08 18:19:56 -0400

    Adds authentication

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware 'Body parser'
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
@@ -17,4 +20,3 @@ app.use('/api/posts', require('./routes/api/posts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
